656039f98a5ac28b994842e24c9f8c17daeade8b
huhahu <358715406@qq.com>
2019-09-30 14:26:54 +0800

    Update suning_goods.sh
    11

--- import_shell/suning_goods.sh
+++ import_shell/suning_goods.sh
@@ -1,1 +1,1 @@
-mysqlimport --ignore-lines=1  --fields-terminated-by=, --local --host 127.0.0.1 -u dw -p ods ~/data/$(date +%Y%m%d)/suning_goods.csv
+mysqlimport --ignore-lines=1  --fields-terminated-by=, --local --host 127.0.0.1 -u dw -p ods ~/data/$(date +%Y%m%d)/suning_goods.csv 
